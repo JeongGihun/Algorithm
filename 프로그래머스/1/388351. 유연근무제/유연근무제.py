9ecb4eda1482dbc37fa6dfc36d91217c49c1af60
JeongGihun <solux3501@gmail.com>
2025-11-27 21:23:41 +0900

    [level 1] Title: 유연근무제, Time: 2.95 ms, Memory: 9.52 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.py
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.py
@@ -4,13 +4,16 @@ def time_change(x) :
         x += 40
     return x
 
+def To_monday(x, startday) :
+    return (x-startday+1)%7
+
 def solution(schedules, timelogs, startday):
     answer = 0
     people = len(schedules)
     for i in range(people) :
         tmp = time_change(schedules[i])
         for j in range(7) :
-            day = (j-startday+1)%7
+            day = To_monday(j, startday)
             if j != 5 and j != 6 :
                 if tmp < timelogs[i][day] :
                     break
