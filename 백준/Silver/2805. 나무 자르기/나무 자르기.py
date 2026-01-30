db1c25e3da8b927c9603c43f9aadf43a55eec925
solux3501 <solux3501@gmail.com>
2026-01-30 21:45:47 +0900

    [Silver II] Title: 나무 자르기, Time: 1696 ms, Memory: 258624 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -3,21 +3,16 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split()) # n 나무 갯수 / m은 필요한 나무 높이
 l = list(map(int, input().split())) # 나무
-s, e = 0, max(l)
-ans = 0
+l.sort(reverse=True)
 
-while s <= e :
-    mid = (s + e) // 2
-    total = 0
-    for i in l :
-        if i > mid :
-            total += (i-mid)
-            if total >= m :
-                break
-
-    if total >= m :
-        ans = mid
-        s = mid + 1
+x = 0 # 위치
+cnt = 1 # 계산하는 수량
+while m > 0 :
+    if x == n-1 or l[x] > l[x+1] :
+        l[x] -= 1
+        m -= cnt
     else :
-        e = mid - 1
-print(ans)
+        cnt += 1
+        x += 1
+
+print(l[x])
