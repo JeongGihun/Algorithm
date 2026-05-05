4b4966f218cf889fcec951d05e2ef3757f69f0bd
solux3501 <solux3501@gmail.com>
2026-05-05 09:41:45 +0900

    Time: 0 ms (100%), Space: 19.4 MB (25.39%) - LeetHub

---
+++ LeetCode/1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
@@ -0,0 +1,15 @@
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        ans = []
+        t = 0
+        for i in range(1, n+1) :
+            ans.append("Push")
+            if i == target[t] :
+                t += 1
+                if t == len(target) :
+                    break
+            else :
+                ans.append("Pop")
+        return ans           
+            
+        
